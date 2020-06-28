4c1b346b6b75653ec12111469290135fa1cf19ed
shakeeb <shakeebanwar250@gmail.com>
2020-06-28 23:26:23 +0500

    weather

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var temp = document.querySelector('.temp');
 
 button.addEventListener('click',function(){
 
-    fetch('api.openweathermap.org/data/2.5/weather?q={inputValue.value}&appid={8d992082f76fa219b9eb3e64cf6e000d}')
+    fetch('api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=8d992082f76fa219b9eb3e64cf6e000d')
 .then(response=>response.json())
 .then(data=>{
 
