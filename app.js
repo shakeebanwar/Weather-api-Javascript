b801d153b8fcaf31ba8874401e573bcecd1702e6
shakeeb <shakeebanwar250@gmail.com>
2020-06-28 23:31:57 +0500

    weather

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ button.addEventListener('click',function(){
     fetch('api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=8d992082f76fa219b9eb3e64cf6e000d')
 .then(response=>response.json())
 .then(data=>{
+alert(data)
+    // var nameValue = data['name'];
+    // var tempValue = data['main']['temp'];
+    // var descValue = data['weather'][0]['description'];
 
-    var nameValue = data['name'];
-    var tempValue = data['main']['temp'];
-    var descValue = data['weather'][0]['description'];
-
-    name.innerHtml = nameValue;
-    temp.innerHTML = tempValue;
-    desc.innerHTML = descValue;
+    // name.innerHtml = nameValue;
+    // temp.innerHTML = tempValue;
+    // desc.innerHTML = descValue;
 })
 
 .catch (err=> alert("wrong city name"))
